f43606e1b07466d497034b389fe8f526f9e307c2
liangfung <1098486429@qq.com>
2019-02-16 21:50:16 +0800

    his

---
+++ historyConfig/webpack.config.css.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyjsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash:8].js',
+    path: path.resolve(__dirname, './dist'), // 绝对路径
+  },
+  devServer: {
+    port: 7000
+  },
+  optimization: {
+    minimizer: [
+      new UglifyjsPlugin({
+        cache: true,
+        sourceMap: true,
+        parallel: true
+      }),
+      new OptimizeCssAssetsPlugin()
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [  // 数组，放插件实例
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true
+      },
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
