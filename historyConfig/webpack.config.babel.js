e269074d15c4e9102cb70193ff8da098fe9184e8
liangfung <1098486429@qq.com>
2019-02-17 22:41:38 +0800

    备份

---
+++ historyConfig/webpack.config.babel.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyjsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash:8].js',
+    path: path.resolve(__dirname, './dist'), // 绝对路径
+  },
+  devServer: {
+    port: 7000
+  },
+  optimization: {
+    minimizer: [
+      new UglifyjsPlugin({
+        cache: true,
+        sourceMap: true,
+        parallel: true
+      }),
+      new OptimizeCssAssetsPlugin()
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                ["@babel/plugin-proposal-class-properties", { "loose": true }],
+                '@babel/plugin-transform-runtime'
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [  // 数组，放插件实例
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true
+      },
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
